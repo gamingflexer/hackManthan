6cfd064d06f7d9d2acf49ee5fa8bd3b3cae52300
AadityaCholayil <cholayilaaditya@gmail.com>
2022-06-14 18:01:32 +0530

    Added Auth and Database Repositories, Added Custom Exceptions

---
+++ App/lib/repositories/database_repository.dart
@@ -0,0 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hackmanthan_app/models/user.dart';
+import 'package:hackmanthan_app/shared/error_screen.dart';
+
+class DatabaseRepository {
+  final String uid;
+
+  DatabaseRepository({required this.uid});
+
+  final FirebaseFirestore db = FirebaseFirestore.instance;
+
+  // Users Collection Reference
+  // Reference allows for easy from and to operations
+  CollectionReference<UserData> get usersRef =>
+      db.collection('users').withConverter<UserData>(
+            fromFirestore: (snapshot, _) => UserData.fromJson(snapshot.data()!),
+            toFirestore: (user, _) => user.toJson(),
+          );
+
+  // Get UserData from DB
+  Future<UserData> get completeUserData async {
+    try {
+      UserData userDataNew = await usersRef
+          .doc(uid)
+          .get()
+          .then((value) => value.data() ?? UserData.empty);
+      return userDataNew;
+    } on Exception catch (_) {
+      throw const SomethingWentWrong();
+    }
+  }
+}
