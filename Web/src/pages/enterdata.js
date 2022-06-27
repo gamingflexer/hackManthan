300c6509640b92a248379c0542844fa7a1cec09b
Abhinavtdk <abhinavch16@gmail.com>
2022-06-28 01:38:49 +0530

    changes

--- Web/src/pages/enterdata.js
+++ Web/src/pages/enterdata.js
@@ -5,9 +5,48 @@ import { CustomerListToolbar } from '../components/customer/customer-list-toolba
 import { DashboardLayout } from '../components/dashboard-layout';
 import { customers } from '../__mocks__/customers';
 import DataForm from 'src/components/dataForm';
+import { useState } from 'react';
 
-const EnterData = () => (
-  <>
+const EnterData = () => {
+
+
+  // const [selectedFile, setSelectedFile] = useState();
+  // const [isFilePicked, setIsFilePicked] = useState(false)
+
+  // const changeHandler = (event) => {
+  //   setSelectedFile(event.target.files[0]);
+  //   console.log(event.target.files[0])
+  //   setIsFilePicked(true);
+  // };
+
+  // const onImportClick = async () => {
+  //   const formData = new FormData();
+  //   console.log(selectedFile)
+  //   formData.append('file', selectedFile);
+  //   // const params = {
+  //   //   file: formData
+  //   // }
+  //   console.log(formData)
+  //   // console.log(JSON.stringify(params))
+  //   fetch(
+  //     'http://20.26.235.201:8888/file-upload',
+  //     {
+  //       method: 'POST',
+  //       body: formData,
+  //     }
+  //   )
+  //     .then((response) => response.json())
+  //     .then((result) => {
+  //       console.log('Success:', result);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error:', error);
+  //     });
+  // };
+
+
+
+  return <div>
     <Head>
       <title>
         Enter Data
@@ -26,21 +65,43 @@ const EnterData = () => (
             display: 'flex',
             flexWrap: 'wrap',
             flexDirection: 'column',
+            width: '80%',
             m: -1
           }}
         >
-          <Typography
-            sx={{ m: 1 }}
-            variant="h4"
-          >
-            Enter Data
-          </Typography>
+          <div style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+          }}>
+            <Typography
+              sx={{ m: 1 }}
+              variant="h4"
+            >
+              Enter Data
+            </Typography>
+            {/* <div style={{
+              cursor: "pointer"
+            }} >
+              <Typography
+                sx={{ m: 1 }}
+                variant="h5"
+                onClick={() => onImportClick()}
+              >
+                Import
+              </Typography>
+              <input type="file" name="file" onChange={changeHandler} />
+            </div> */}
+          </div>
+
           <DataForm />
         </Box>
       </Container>
     </Box>
-  </>
-);
+  </div>
+
+}
 EnterData.getLayout = (page) => (
   <DashboardLayout>
     {page}
